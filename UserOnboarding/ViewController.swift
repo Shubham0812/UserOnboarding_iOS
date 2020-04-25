f7b8c5bba403bb20056df685122aede09056905a
Shubham0812 <singh.shubham0812@gmail.com>
2020-04-25 19:58:27 +0530

    Comments added

--- UserOnboarding/ViewController.swift
+++ UserOnboarding/ViewController.swift
@@ -10,44 +10,45 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    //MARK:- outlets for the viewController
+    // MARK:- outlets for the viewController
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var getStartedButton: UIButton!
     
-    
+    // data for the Onboarding Screens
     let pages: [Page] = [Page(animationName: "animation1", title: "Learn to Code", description: "Find awesome tutorials on how to code and improve your coding practices"), Page(animationName: "animation2", title: "Code with Friends", description: "Practice with friends and solve problems together to earn points"),  Page(animationName: "animation3", title: "Always there to Help", description: "Having Trouble? Get guidance from our experienced Mentors")]
     
     
-    //MARK:- lifeCycle methods for the ViewController
+    // MARK:- lifeCycle methods for the ViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.getStartedButton.layer.cornerRadius = 20
         
-        pageControl.translatesAutoresizingMaskIntoConstraints = true
-
+        // for making the get started button rounded
+        self.getStartedButton.layer.cornerRadius = 20
+    
+        // customizing the collectionView
         self.collectionView.backgroundColor = .white
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
-        self.pageControl.currentPage = 0
-
-    
         self.collectionView.register(UINib(nibName: OnboardingCollectionViewCell.identifier, bundle: Bundle.main), forCellWithReuseIdentifier: "OnboardingCollectionViewCell")
     }
     
-    //MARK:- outlet functions for the viewController
+    // MARK:- outlet functions for the viewController
     @IBAction func pageChanged(_ sender: Any) {
         let pc = sender as! UIPageControl
+        
+        // scrolling the collectionView to the selected page
         collectionView.scrollToItem(at: IndexPath(item: pc.currentPage, section: 0), at: .centeredHorizontally, animated: true)
     }
     
     @IBAction func getStartedButtonTapped(_ sender: Any) {
-        print("Move to HomeViewController")
+        // move the user to the other view controller
+        print("Move to other view controller")
     }
     
     
     
-    //MARK:- collectionView dataSource & collectionView FlowLayout delegates
+    // MARK:- collectionView dataSource & collectionView FlowLayout delegates
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return pages.count
     }
@@ -58,11 +59,11 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return cell
     }
     
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: self.collectionView.frame.width, height: self.collectionView.frame.height)
     }
     
+    // for updating the page control
     func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         pageControl.currentPage = collectionView.indexPathsForVisibleItems[0].item
     }
