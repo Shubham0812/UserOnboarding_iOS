11ba61090b732cdd419d93abba522b44909afee0
Shubham0812 <singh.shubham0812@gmail.com>
2020-04-26 15:50:20 +0530

    Comments added for the ViewController

--- UserOnboarding/ViewController.swift
+++ UserOnboarding/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController,  UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     // MARK:- outlets for the viewController
     @IBOutlet weak var collectionView: UICollectionView!
@@ -16,21 +16,27 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var getStartedButton: UIButton!
     
     // data for the Onboarding Screens
-    let pages: [Page] = [Page(animationName: "animation1", title: "Learn to Code", description: "Find awesome tutorials on how to code and improve your coding practices"), Page(animationName: "animation2", title: "Code with Friends", description: "Practice with friends and solve problems together to earn points"),  Page(animationName: "animation3", title: "Always there to Help", description: "Having Trouble? Get guidance from our experienced Mentors")]
+    let pages: [Page] = [Page(animationName: "animation1", title: "Learn to Code", description: "Find awesome tutorials on how to code and improve your coding practices"),
+                         Page(animationName: "animation2", title: "Code with Friends", description: "Practice with friends and solve problems together to earn points"),
+                         Page(animationName: "animation3", title: "Always there to Help", description: "Having Trouble? Get guidance from our experienced Mentors")]
     
     
     // MARK:- lifeCycle methods for the ViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // for making the get started button rounded
+        // to make the button rounded
         self.getStartedButton.layer.cornerRadius = 20
-    
-        // customizing the collectionView
+        
+        // register the custom CollectionViewCell and assign the delegates to the ViewController
         self.collectionView.backgroundColor = .white
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
-        self.collectionView.register(UINib(nibName: OnboardingCollectionViewCell.identifier, bundle: Bundle.main), forCellWithReuseIdentifier: "OnboardingCollectionViewCell")
+        self.collectionView.register(UINib(nibName: OnboardingCollectionViewCell.identifier, bundle: Bundle.main),
+                                     forCellWithReuseIdentifier: "OnboardingCollectionViewCell")
+        
+        // set the number of pages to the number of Onboarding Screens
+        self.pageControl.numberOfPages = self.pages.count
     }
     
     // MARK:- outlet functions for the viewController
@@ -38,7 +44,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         let pc = sender as! UIPageControl
         
         // scrolling the collectionView to the selected page
-        collectionView.scrollToItem(at: IndexPath(item: pc.currentPage, section: 0), at: .centeredHorizontally, animated: true)
+        collectionView.scrollToItem(at: IndexPath(item: pc.currentPage, section: 0),
+                                    at: .centeredHorizontally, animated: true)
     }
     
     @IBAction func getStartedButtonTapped(_ sender: Any) {
@@ -54,19 +61,22 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier,
+                                                      for: indexPath) as! OnboardingCollectionViewCell
+        // function for configuring the cell, defined in the Custom cell class
         cell.configureCell(page: pages[indexPath.item])
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: self.collectionView.frame.width, height: self.collectionView.frame.height)
     }
     
-    // for updating the page control
-    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    // to update the UIPageControl
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell,
+                        forItemAt indexPath: IndexPath) {
         pageControl.currentPage = collectionView.indexPathsForVisibleItems[0].item
     }
-    
 }
 
